6b0d120561ed50a0bc80c279c72219b57f6cd7a2
Rafa-Lino <rafaelino71@gmail.com>
2022-12-19 15:22:03 -0300

    Finalização do projeto

--- proway-computers/src/app/contato/contato.module.ts
+++ proway-computers/src/app/contato/contato.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
 import { ReactiveFormsModule } from '@angular/forms';
